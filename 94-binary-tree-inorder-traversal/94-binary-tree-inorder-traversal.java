8172c0efb686fe81f437d6d5153639815b88bcbe
vinay d <vinay.chowdary518@gmail.com>
2022-08-21 14:41:44 -0700

    Time: 1 ms (50.61%), Space: 42.1 MB (68.75%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
@@ -16,14 +16,46 @@
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> result = new ArrayList<Integer>();
-        inorderTraversal(root, result);
+        
+        if (root != null) {
+            // recursive way
+            // inorderTraversalRecursive(root, result);
+            
+            // non-recursive way
+            result = inorderTraversalNonRecursive(root);
+        }
+        
         return result;
     }
-    public void inorderTraversal(TreeNode root, List<Integer> result) {
-        if (root != null) {
-            inorderTraversal(root.left, result);
-            result.add(root.val);
-            inorderTraversal(root.right, result);
+    
+    // non-recursive way
+    public List<Integer> inorderTraversalNonRecursive(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        
+        TreeNode currentNode = root;
+        while (currentNode != null || stack.size() > 0) {
+            // travel left subtree
+            while (currentNode != null) {
+                stack.push(currentNode);
+                currentNode = currentNode.left;
+            }
+            
+            // take last node as currentNode is null
+            currentNode = stack.pop();
+            result.add(currentNode.val);
+            
+            // travle righ subtree
+            currentNode = currentNode.right;
         }
+        
+        return result;
+    }
+    
+    // recursive way
+    public void inorderTraversalRecursive(TreeNode root, List<Integer> result) {
+        inorderTraversalRecursive(root.left, result);
+        result.add(root.val);
+        inorderTraversalRecursive(root.right, result);
     }
 }
