483117a2a6f04360aed95b6e0f083873e066d1b1
vinay d <vinay.chowdary518@gmail.com>
2022-10-29 23:29:57 -0700

    Time: 1295 ms (13.22%), Space: 47.3 MB (27.79%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.replaceAll("[^a-zA-Z0-9]","");
+        s = s.toLowerCase();
+        int len = s.length();
+        for(int i=0;i<len/2;i++){
+            if(s.charAt(i)==s.charAt(len-1-i)){
+                continue;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
