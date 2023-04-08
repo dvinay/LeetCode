420a6cbe3796ccc1792621a3484393201e25bd0f
vinay d <vinay.chowdary518@gmail.com>
2023-04-08 11:56:39 -0700

    Time: 12 ms (70.58%), Space: 54.8 MB (87.30%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -1,10 +1,11 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Map<Integer, Integer> countMap = new HashMap<Integer, Integer>();
+        Set<Integer> set = new HashSet<Integer>();
         
         for (int num:nums) {
-            countMap.put(num, countMap.getOrDefault(num, 0)+1);
-            if (countMap.get(num) >= 2) {
+            if (!set.contains(num)) {
+                set.add(num);
+            } else {
                 return true;
             }
         }
