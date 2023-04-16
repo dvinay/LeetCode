bc3776b9a444132077d71d8e0c9b001324a14cc1
vinay d <vinay.chowdary518@gmail.com>
2023-04-15 17:18:23 -0700

    Time: 2 ms (61.67%), Space: 42.4 MB (86.46%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.java
+++ 152-maximum-product-subarray/152-maximum-product-subarray.java
@@ -1,5 +1,45 @@
 class Solution {
     public int maxProduct(int[] nums) {
+        if (nums == null || nums.length <= 0) {
+            return 0;
+        }
+        int minProduct = nums[0];
+        int maxProduct = nums[0];
+        int result = nums[0];
+        for (int index = 1 ; index < nums.length ; index++) {
+            if (nums[index] < 0) {
+                int temp = maxProduct;
+                maxProduct = minProduct;
+                minProduct = temp;
+            }
+            minProduct = Math.min(nums[index], nums[index]*minProduct);
+            maxProduct = Math.max(nums[index], nums[index]*maxProduct);
+            result = Math.max(maxProduct, result);
+        }
+        return result;
+        
+        // ***** current product approach *****
+        /*
+        if (nums == null || nums.length <= 0) {
+            return 0;
+        }
+        int minProduct = nums[0];
+        int maxProduct = nums[0];
+        int result = nums[0];
+        
+        for (int index = 1 ; index < nums.length ; index++) {
+            int temp = maxProduct;
+            maxProduct = Math.max(Math.max(nums[index]*minProduct, nums[index]*maxProduct), nums[index]);
+            minProduct = Math.min(Math.min(nums[index]*minProduct, nums[index]*temp), nums[index]);
+            
+            result = Math.max(maxProduct, result);
+        }
+        
+        return result;
+        */
+        
+        // ***** traversal approach *****
+        /*
         int result = Integer.MIN_VALUE;
         int product = 1;
         
@@ -21,5 +61,6 @@ class Solution {
         }
         
         return result;
+        */
     }
 }
