c18f36cd436e2e2dc5354ecdc96509b0511ff04a
vinay d <vinay.chowdary518@gmail.com>
2023-06-10 20:28:33 -0700

    Time: 9 ms (49.19%), Space: 44.4 MB (26.72%) - LeetHub

--- 6-zigzag-conversion/6-zigzag-conversion.java
+++ 6-zigzag-conversion/6-zigzag-conversion.java
@@ -4,22 +4,57 @@ class Solution {
             return s;
         }
         
+        StringBuffer[] rows = new StringBuffer[numRows];
         StringBuffer result = new StringBuffer();
-        for (int i=0;i<numRows;i++) {
-            int nextPosition = (numRows-1)*2;
-            for (int j=i;j<s.length();j=j+nextPosition) {
-                result.append(s.charAt(j));
+        
+        int index = 0;
+        
+        // initialize the rows
+        for (int row=0;row<numRows;row++) {
+            rows[row] = new StringBuffer();
+        }
+        
+        // Prepare each row with context
+        while (index<s.length()) {
+            //downward direction filling
+            for (int row=0;row<numRows && index<s.length();row++) {
+                rows[row].append(s.charAt(index++));
+            }
+            //upward direction filling
+            for (int row=numRows-2;row>0 && index<s.length();row--) {
+                rows[row].append(s.charAt(index++));
+            }
+        }
+        
+        for (int row=0;row<numRows;row++) {
+            result.append(rows[row]);
+        }
+        return result.toString();
+        
+        /* 
+        Mathematical approach: 
+        Each horizontal positions = 2 * (number of rows - 1)
+        Each Cross positions = 2 * (number of rows - 1) - 2 * current row
+        Note: Each Cross positions computaiton not required for fist and last row.
+        
+        StringBuffer result = new StringBuffer();
+        for (int row=0;row<numRows;row++) {
+            int nextHorizontalPosition = (numRows-1)*2;
+            for (int i=row;i<s.length();i=i+nextHorizontalPosition) {
+                result.append(s.charAt(i));
                 
-                //todo: in between lines
-                int midPosition = j+nextPosition-i*2;
-                if (i > 0 && i < numRows - 1 
-                    && midPosition > 0
-                    && midPosition < s.length()) {
-                    result.append(s.charAt(midPosition));
+                //next character position which is in-between the horizontal lines
+                int nextCrossPosition = (i+nextHorizontalPosition)-(row*2);
+                //next Cross Position is not require for first and last lines
+                if (row > 0 && row < numRows - 1 
+                    && nextCrossPosition > 0
+                    && nextCrossPosition < s.length()) {
+                    result.append(s.charAt(nextCrossPosition));
                 }
                 
             }
         }
         return result.toString();
+        */
     }
 }
